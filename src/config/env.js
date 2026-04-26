672f1559fd8c31ce0320fdc8c2a2d8ece792ed4c
Sidhique <xidhique@gmail.com>
2026-04-26 22:50:38 +0530

    Third

--- src/config/env.js
+++ src/config/env.js
@@ -5,7 +5,7 @@ const env = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://Erpsystem:erpSystem@cluster0.bc2yuc7.mongodb.net/vantus_erp?appName=Cluster0',
   JWT_SECRET: process.env.JWT_SECRET || 'dev-secret-change-me',
   JWT_EXPIRES: process.env.JWT_EXPIRES || '7d',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://erp-frontent.vercel.app',
   NODE_ENV: process.env.NODE_ENV || 'development',
 };
 
